19af00058fe342f9151cb41e33ad37bb144f13bd
manav1999 <manavkapil11@gmail.com>
2022-09-28 08:03:27 +0000

    ADD: search

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -87,6 +88,24 @@ func DeleteRecipeHandler(c *gin.Context) {
 
 }
 
+func SearchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecepies := make([]Recipe, 0)
+	for i := 0; i < len(recipes); i++ {
+		found := false
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
+			}
+		}
+		if found {
+			listOfRecepies = append(listOfRecepies, recipes[i])
+		}
+	}
+
+	c.JSON(http.StatusOK, listOfRecepies)
+}
+
 func main() {
 	router := gin.Default()
 
@@ -94,6 +113,7 @@ func main() {
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchRecipesHandler)
 
 	router.Run()
 }
